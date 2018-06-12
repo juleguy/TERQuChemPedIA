9e0501fe2c78027e7b372130e6f7ff8bf6426d89
jules.leguy <jules.leguy55@gmail.com>
2018-06-12 09:46:42 +0200

    Suppression du chemin local dans quchem_ia.py

--- quchem_ia/quchem_ia.py
+++ quchem_ia/quchem_ia.py
@@ -650,8 +650,6 @@ def execute(json_path):
                 _grid_search_cv(task[grid_search_cv_k])
 
 
-execute("jsons/exec_9.json")
-
 # Executing given json files
 for arg in sys.argv[1:]:
     print(sys.argv)
